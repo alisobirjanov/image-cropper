cf03341ca3578818224532e0eafa341e1e03abb7
Alimardon <alisobirzanov29@gmail.com>
2023-10-17 22:46:45 +0500

    removed

--- vite.config.ts
+++ vite.config.ts
@@ -6,35 +6,3 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()]
 });
-
-
-
-
-function myPlugin() {
-  const virtualModuleId = 'virtual:my-module';
-  const resolvedVirtualModuleId = '\0' + virtualModuleId;
-
-  return {
-    name: 'my-plugin', // required, will show up in warnings and errors
-    resolveId(id) {
-      console.log(id)
-      if (id.endsWith('.lazy')) {
-        return id;
-      }
-    },
-    load(id: string) {
-      // console.log(id.startsWith('lazy:'));
-      if (id.endsWith('.lazy')) {
-        console.log(id);
-        const res = `
-          <div>
-            HIII
-          </div>
-
-      `;
-        console.log(res);
-        return res;
-      }
-    },
-  };
-}
